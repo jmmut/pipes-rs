6ded662586aafed0dbcfccb6cf6bcb5906e24406
jose miguel mut <jomutlo@gmail.com>
2023-10-28 23:59:18 +0100

    allow reading from a file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crate::common::AnyError;
 use clap::Parser;
+use std::path::PathBuf;
 
 use crate::evaluate::Runtime;
 use crate::frontend::ast::Ast;
@@ -12,7 +13,12 @@ mod frontend;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    code_string: String,
+    /// inline pipes code to be interpreted. Either this or the input file must be provided
+    code_string: Option<String>,
+
+    /// input file with pipes code to interpret. Either this or the code string must be provided
+    #[arg(short, long)]
+    input_file: Option<PathBuf>,
 
     #[arg(short, long)]
     prettify: bool,
@@ -35,10 +41,11 @@ fn main() -> Result<(), AnyError> {
 fn interpret() -> Result<(), AnyError> {
     let Args {
         code_string,
+        input_file,
         prettify,
         alternative,
     } = Args::parse();
-
+    let code_string = read_input(code_string, input_file)?;
     let expression = if alternative {
         Ast::deserialize(&code_string)?
     } else {
@@ -54,4 +61,19 @@ fn interpret() -> Result<(), AnyError> {
     let result = Runtime::evaluate(expression)?;
     println!("{}", result);
     Ok(())
+}
+
+fn read_input(
+    code_string: Option<String>,
+    input_file: Option<PathBuf>,
+) -> Result<String, AnyError> {
+    if code_string.is_some() && input_file.is_some() {
+        Err("Only the code string or the input file should be provided")?
+    } else if let Some(code) = code_string {
+        Ok(code)
+    } else if let Some(file) = input_file {
+        Ok(std::fs::read_to_string(file)?)
+    } else {
+        Err("Either the code string or the input file should be provided")?
+    }
 }
