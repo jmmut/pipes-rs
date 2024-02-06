593772c8a3df3b01c7ab7e40d5722d4755fe6fda
jose miguel mut <jomutlo@gmail.com>
2024-02-06 23:26:17 +0000

    add typecheck for arithmetic operations

--- src/typing.rs
+++ src/typing.rs
@@ -51,6 +51,28 @@ pub mod builtin_types {
     pub const I64: Type = Type::Builtin { type_name: "i64" };
 }
 
+fn type_mismatch<T>(
+    actual_expression: &Expression,
+    actual: &Type,
+    expected: &Type,
+) -> Result<T, AnyError> {
+    err(format!(
+        "Type mismatch for expression '{:?}':\
+        \n  actual:   {:?}\
+        \n  expected: {:?}",
+        actual_expression, actual, expected
+    ))
+}
+
+fn assert_same_type(actual_expression: &Expression, expected: &Type) -> Result<Type, AnyError> {
+    let actual_type = get_type(actual_expression)?;
+    if actual_type != *expected {
+        type_mismatch(actual_expression, &actual_type, expected)
+    } else {
+        Ok(actual_type)
+    }
+}
+
 fn check_types_chain(chain: &Chain) -> Result<Type, AnyError> {
     let accumulated = &chain.initial;
     let mut accumulated_type = get_type(accumulated.as_ref())?;
@@ -61,15 +83,10 @@ fn check_types_chain(chain: &Chain) -> Result<Type, AnyError> {
         } = t
         {
             if accumulated_type != *expected_type {
-                return err(format!(
-                    "Type mismatch:\
-                    \n  actual:   {:?}\
-                    \n  expected: {:?}",
-                    accumulated_type, expected_type
-                ));
+                return type_mismatch(accumulated, &accumulated_type, expected_type);
             }
         } else {
-            accumulated_type = get_operation_type(accumulated, t.operator, &t.operand);
+            accumulated_type = get_operation_type(accumulated, t.operator, &t.operand)?;
         }
     }
     Ok(accumulated_type)
@@ -105,7 +122,30 @@ fn get_type(expression: &Expression) -> Result<Type, AnyError> {
     }
 }
 
-fn get_operation_type(input: &Expression, operator: Operator, operand: &Expression) -> Type {
+fn get_operation_type(
+    input: &Expression,
+    operator: Operator,
+    operand: &Expression,
+) -> Result<Type, AnyError> {
+    match operator {
+        Operator::Add
+        | Operator::Substract
+        | Operator::Multiply
+        | Operator::Divide
+        | Operator::Modulo => {
+            assert_same_type(input, &builtin_types::I64)?;
+            assert_same_type(operand, &builtin_types::I64)?;
+            return Ok(builtin_types::I64);
+        }
+        Operator::Ignore => return get_type(operand),
+        Operator::Call => {}
+        Operator::Get => {}
+        Operator::Type => {}
+        Operator::Assignment => {}
+        Operator::Overwrite => {}
+        Operator::Concatenate => {}
+        Operator::Comparison(_) => {}
+    }
     unimplemented!()
 }
 
@@ -142,6 +182,18 @@ mod tests {
     #[test]
     fn test_basic_function_type() {
         let program = &parse("function{4} :function()(:i64)");
+        assert_ok(check_types(program))
+    }
+
+    #[test]
+    fn test_basic_operation() {
+        let program = &parse("4 +3 -1 |* 5 |/ 3 %2 :i64");
+        assert_ok(check_types(program))
+    }
+
+    #[test]
+    fn test_basic_array() {
+        let program = &parse("[ 1 2 ] :array(num :i64)");
         assert_ok(check_types(program))
     }
 }
