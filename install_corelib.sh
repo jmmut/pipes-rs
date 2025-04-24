9b5097907b9d2ae354216ec7ffbaa511171768b6
jose miguel mut <jomutlo@gmail.com>
2025-04-24 04:37:50 +0200

    fix tests (also in CI)

--- install_corelib.sh
+++ install_corelib.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo $SCRIPT_DIR
+
 mkdir -p  ~/.local/share/pipes/
 
-ln -sfT ${PWD}/pipes_programs/corelib ~/.local/share/pipes/core
+ln -sfT "${SCRIPT_DIR}/pipes_programs/corelib" ~/.local/share/pipes/core
 
 
