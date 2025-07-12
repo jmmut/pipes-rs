191718e72a6e3b25f9e6fb7fdafde2da4fd885d4
jose miguel mut <jomutlo@gmail.com>
2025-07-12 11:38:41 +0200

    fix CI

--- web/recompile.sh
+++ web/recompile.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}/.."
 
-# Don't run this manually, run local_build_and_run.sh instead to run the project locally.
+# Don't run this manually, run ./web/run_local.sh from the root instead to run the project locally.
 # This script exists just to make sure the CI and the local building does the same.
 
 mkdir -p ./web/built_html
-wasm-pack build --target web --out-dir ./web/built_html/pkg/
+wasm-pack build interpreter --target web --out-dir ../web/built_html/pkg/
 
 cp ./web/index.html ./web/built_html/
